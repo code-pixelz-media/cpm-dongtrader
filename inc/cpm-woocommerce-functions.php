f86cdaa68217968cf9c96099b441c9310e33da27
anilpanthi <apanthi21@gmail.com>
2023-01-05 12:33:39 +0545

    Adding qrtiger generator to my accounts page

--- inc/cpm-woocommerce-functions.php
+++ inc/cpm-woocommerce-functions.php
@@ -1,28 +1,33 @@
 <?php
 
-// add menu link
-add_filter('woocommerce_account_menu_items', 'dongtrater_qr_generate', 40);
-function dongtrater_qr_generate($menu_links)
+add_filter('woocommerce_account_menu_items', 'cpm_dong_generateqr', 40);
+function cpm_dong_generateqr($menu_links)
 {
 
     $menu_links = array_slice($menu_links, 0, 5, true)
-        + array('generate-qr' => 'Generate Qr Code')
+        + array('generate-qrtiger' => 'Generate Qr Code')
         + array_slice($menu_links, 5, NULL, true);
 
     return $menu_links;
 }
 // register permalink endpoint
-add_action('init', 'dongtrader_qr_endpoint');
-function dongtrader_qr_endpoint()
+add_action('init', 'cpm_dong_add_endpoint');
+function cpm_dong_add_endpoint()
 {
 
-    add_rewrite_endpoint('generate-qr', EP_PAGES);
+    add_rewrite_endpoint('generate-qrtiger', EP_PAGES);
 }
 // content for the new page in My Account, woocommerce_account_{ENDPOINT NAME}_endpoint
-add_action('woocommerce_account_generate-qr', 'misha_my_account_endpoint_content');
-function misha_my_account_endpoint_content()
+add_action('woocommerce_account_generate-qrtiger_endpoint', 'cpm_dong_my_account_endpoint_content');
+function cpm_dong_my_account_endpoint_content()
 {
 
     // of course you can print dynamic content here, one of the most useful functions here is get_current_user_id()
-    echo 'Generate Qr Code here';
+?>
+    <div class='qr-tiger-code-generator'>
+        <p><?php _e('Generate Qr Code ', 'cpm-dongtrader') ?></p>
+        <button class="btn btn-primary qrtiger-code-gen" type="submit"><?php _e('Generate', 'cpm-dongtrader') ?></button>
+    </div>
+
+<?php
 }
