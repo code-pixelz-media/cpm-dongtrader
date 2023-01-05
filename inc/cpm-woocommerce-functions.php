4aade89023557d169c263521f1b0849b6d326c8b
anilpanthi <apanthi21@gmail.com>
2023-01-05 10:04:32 +0545

    adding tabs on my account page for qr code geneartion

--- inc/cpm-woocommerce-functions.php
+++ inc/cpm-woocommerce-functions.php
@@ -1,33 +1,28 @@
 <?php
 
-// Add field
-add_action('woocommerce_edit_account_form', 'misha_add_field_edit_account_form');
-// or add_action( 'woocommerce_edit_account_form_start', 'misha_add_field_edit_account_form' );
-function misha_add_field_edit_account_form()
+// add menu link
+add_filter('woocommerce_account_menu_items', 'dongtrater_qr_generate', 40);
+function dongtrater_qr_generate($menu_links)
 {
 
-    woocommerce_form_field(
-        'phone_number',
-        array(
-            'type'        => 'tel',
-            'required'    => true, // remember, this doesn't make the field required, just adds an "*"
-            'label'       => 'Phone number'
-        ),
-        get_user_meta(get_current_user_id(), 'phone_number', true) // get the data
-    );
+    $menu_links = array_slice($menu_links, 0, 5, true)
+        + array('generate-qr' => 'Generate Qr Code')
+        + array_slice($menu_links, 5, NULL, true);
+
+    return $menu_links;
 }
-// Save field value
-add_action('woocommerce_save_account_details', 'misha_save_account_details');
-function misha_save_account_details($user_id)
+// register permalink endpoint
+add_action('init', 'dongtrader_qr_endpoint');
+function dongtrader_qr_endpoint()
 {
 
-    update_user_meta($user_id, 'phone_number', wc_clean($_POST['phone_number']));
+    add_rewrite_endpoint('generate-qr', EP_PAGES);
 }
-// Make it required
-add_filter('woocommerce_save_account_details_required_fields', 'misha_make_field_required');
-function misha_make_field_required($required_fields)
+// content for the new page in My Account, woocommerce_account_{ENDPOINT NAME}_endpoint
+add_action('woocommerce_account_log-history_endpoint', 'misha_my_account_endpoint_content');
+function misha_my_account_endpoint_content()
 {
 
-    $required_fields['phone_number'] = 'Phone number';
-    return $required_fields;
+    // of course you can print dynamic content here, one of the most useful functions here is get_current_user_id()
+    echo 'Generate Qr Code here';
 }
