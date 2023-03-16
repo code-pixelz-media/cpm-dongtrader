f34361b73838d652a187846b105eff4034d7e99c
anilpanthi <apanthi21@gmail.com>
2023-03-16 10:28:36 +0545

    cron debug 2

--- inc/cpm-dongtrader-msc-functions.php
+++ inc/cpm-dongtrader-msc-functions.php
@@ -34,7 +34,6 @@ function glassfrog_api_management()
     $table_name    = $wpdb->prefix . 'manage_users_gf';
     //get glassfrog id and user id from custom table manage_users_gf
     $results       = $wpdb->get_results("SELECT gf_person_id , user_id FROM $table_name WHERE in_circle = 0 LIMIT 5",ARRAY_A);
-    var_dump($results);
     //if not results exit
     if(!$results) return;
     //extract glssfrog id from the results in the above custom query
@@ -47,6 +46,7 @@ function glassfrog_api_management()
     foreach($all_users as $gfid=>$uid){
         //call the glassfrog api
         $api_call = glassfrog_api_request('people/'.$gfid.'/roles','' , 'GET'); 
+        var_dump($api_call);
         //check if api call is all good       
         if($api_call) :
             //get all people of the circle
